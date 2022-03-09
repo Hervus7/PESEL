a3cf3d0459d1b58eb447c1c2369b0205275410b7
Stanisław <35169070+vuxeim@users.noreply.github.com>
2022-03-09 23:08:31 +0000

    Filter out invalid dates

--- pesel.py
+++ pesel.py
@@ -21,13 +21,13 @@ def check_digit(pesel: str) -> int:
 def is_valid(pesel: str) -> bool:
     """
     Checks if string is a valid PESEL. Criterias are:
-    11 chars long,
-    correct check digit,
-    month and day are both not equal to 0 (makes '00000000000' invalid)
+    - 11 chars long,
+    - correct check digit,
+    - day, month and year combination is possible
     """
-    if len(pesel) == 11:
+    if len(pesel) == 11 and pesel.isnumeric():
         if int(pesel[10]) == check_digit(pesel):
-            if all([int(pesel[i*2:i*2+2]) for i in range(1, 3)]):
+            if is_valid_date(*get_birth_date(pesel)):
                 return True
     return False
 
@@ -39,35 +39,45 @@ def is_female(pesel: str) -> bool:
     """
     return int(pesel[9]) % 2 == 0
 
-def birth_date(pesel: str) -> list:
+def get_birth_date(pesel: str) -> list:
     """ Returns birth date from a PESEL """
     century, month = divmod(int(pesel[2:4]), 20)
-    day = pesel[4:6]
-    year = f'{CENTURIES.get(century)}{pesel[0:2]}'
-    month = str(month).zfill(2)
+    day = int(pesel[4:6])
+    year = int(f'{CENTURIES.get(century)}{pesel[0:2]}')
+    month = int(month)
     return day, month, year
+
+def is_valid_date(day: int, month: int, year: int) -> bool:
+    """ Checks if day, month and year combination is a valid date """
+    max_day = 31 if month != 2 else 31-(2+int(not is_leap(int(year))))
+    # PESEL system is suitable for handling years only from 1800 to 2299
+    conditions = [(1 <= day <= max_day), (1 <= month <= 12), (1800 <= year <= 2299)]
+    return all(conditions)
 
 def gen_pesel(day=None, month=None, year=None, female=None) -> str:
     """ Generates random valid PESEL """
     year = str(random.randint(1800, 2299)) if not year else str(year)
     month = random.randint(1, 12) if not month else int(month)
     max_day = 31 if month != 2 else 31-(2+int(not is_leap(int(year))))
-    if month in (4, 6, 9, 11) : max_day = 30
-    day = str(random.randint(0, max_day)).zfill(2) if not day else str(day)
+    if month in (4, 6, 9, 11):
+        max_day = 30
+    day = str(random.randint(1, max_day)).zfill(2) if not day else str(day)
     cent_month = str(CENTURIES_REVERSED.get(int(year[0:2]))*20+month).zfill(2)
     gender = random.randint(0, 9999)
-    if female != None : gender = gender-gender%2 if female else gender-gender%2+1
+    if female != None:
+        gender = gender-gender%2 if female else gender-gender%2+1
     parts = year[2:4]+str(cent_month)+day+str(gender).zfill(4)
     parts += str(check_digit(parts))
     return parts
 
 def show_info(pesel: str) -> None:
     """ Prints info in a nice way """
-    gender = 'female' if is_female(pesel) else 'male'
+    gender = '-' if len(pesel) < 10 else 'female' if is_female(pesel) else 'male'
+    date = get_birth_date(pesel) if len(pesel) >= 6 else '---'
     print('--- '+pesel+' ---')
-    print('Valid : ', is_valid(pesel))
+    print('Valid : ', 'yes' if is_valid(pesel) else 'no')
     print('Gender : ', gender)
-    print('Birth date : day: {} month: {} year: {}'.format(*birth_date(pesel)))
+    print('Birth date : day: {} month: {} year: {}'.format(*date))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Check if string is a valid PESEL or generate random valid PESEL with specified parameters')
@@ -79,4 +89,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     pesel = args.pesel if args.pesel else gen_pesel(args.day, args.month, args.year, args.female)
-    show_info(pesel)
+    if pesel.isnumeric():
+        show_info(pesel)
+    else:
+        print('PESEL cannot contain letters')
